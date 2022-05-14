994429d3b440f361eb076b9c62841f4299e252b0
Maylhi <maylhi12.01@gmail.com>
2022-05-13 22:11:27 -0500

    Se agrega el proyecto

---
+++ Pruebas/Repositories/LibroRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using CalidadT2.Models;
+using CalidadT2.Repositories;
+using Moq;
+using NUnit.Framework;
+using Pruebas.Repositories.Mock;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pruebas.Repositories
+{
+    class LibroRepositoryTest
+    {
+        private Mock<AppBibliotecaContext> mockContext;
+
+        
+        [SetUp]
+        public void SetUp()
+        {
+            mockContext = AplicationContextMock.GetApplicationContextMock();
+        }
+
+        [Test]
+        public void TestGetAllLibros()
+        {
+            var repository = new LibroRepository(mockContext.Object);
+            var libros = repository.GetAllLibros();
+
+            Assert.IsNotNull(libros);
+        }
+        [Test]
+        public void TestDetailLibro()
+        {
+            var repository = new LibroRepository(mockContext.Object);
+            var libro = repository.DetailLibro(1);
+
+            Assert.IsNotNull(libro);
+            Assert.AreEqual(libro.Nombre,"pinocho");
+        }
+        [Test]
+        public void TestGetLibroById()
+        {
+            var repository = new LibroRepository(mockContext.Object);
+            var libro = repository.GetLibroById(1);
+
+            Assert.IsNotNull(libro);
+            Assert.AreEqual(libro.Nombre, "pinocho");
+        }
+
+    }
+}
---
+++ Pruebas/Repositories/Mock/AplicationContextMock.cs
@@ -0,0 +1,51 @@
+﻿using CalidadT2.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pruebas.Repositories.Mock
+{
+    class AplicationContextMock
+    {
+        public static Mock<AppBibliotecaContext> GetApplicationContextMock()
+        {
+            
+
+            IQueryable<Libro> libroData = GetLibroData();
+
+            var mockDbSetLibro = new Mock<DbSet<Libro>>();
+            mockDbSetLibro.As<IQueryable<Libro>>().Setup(m => m.Provider).Returns(libroData.Provider);
+            mockDbSetLibro.As<IQueryable<Libro>>().Setup(m => m.Expression).Returns(libroData.Expression);
+            mockDbSetLibro.As<IQueryable<Libro>>().Setup(m => m.ElementType).Returns(libroData.ElementType);
+            mockDbSetLibro.As<IQueryable<Libro>>().Setup(m => m.GetEnumerator()).Returns(libroData.GetEnumerator());
+            mockDbSetLibro.Setup(m => m.AsQueryable()).Returns(libroData);
+
+
+
+            var mockContext = new Mock<AppBibliotecaContext>(new DbContextOptions<AppBibliotecaContext>());
+            mockContext.Setup(c => c.Libros).Returns(mockDbSetLibro.Object);
+
+            return mockContext;
+        }
+
+        
+
+        private static IQueryable<Libro> GetLibroData()
+        {
+            return new List<Libro>
+            {
+                new Libro {
+                    Id = 1,
+                    Nombre = "pinocho"
+                },
+                new Libro {
+                    Id = 1,
+                    Nombre = "harry potter"
+                },
+            }.AsQueryable();
+        }
+    }
+}
